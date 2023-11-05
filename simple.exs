3f26d490cc609d64ecf7c7301e34ad0e43c23888
Robb Shecter <robb@public.law>
2023-11-05 00:03:21 -0600

    use CLI

--- simple.exs
+++ simple.exs
@@ -60,7 +60,7 @@ code_abbrevs = %{
 }
 
 
-url      = "https://www.cde.ca.gov/sp/ch/qandasec5.asp"
+[url]    = System.argv() # "https://www.cde.ca.gov/sp/ch/qandasec5.asp"
 response = HTTPoison.get!(url)
 pattern  = ~r/sectionNum=[^&]+&lawCode=\w\w\w/
 
