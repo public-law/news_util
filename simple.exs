6c53364f16e2606d999454e99793e357d9785ab9
Robb Shecter <robb@public.law>
2023-11-05 00:00:14 -0600

    handle more abbrevs

--- simple.exs
+++ simple.exs
@@ -2,6 +2,7 @@ cal_codes = %{
   "CONS" => "Constitution",
   "BPC"  => "Business and Professions Code",
   "CIV"  => "Civil Code",
+  "CCP"  => "Code of Civil Procedure",
   "COM"  => "Commercial Code",
   "CORP" => "Corporations Code",
   "EDC"  => "Education Code",
@@ -29,7 +30,33 @@ cal_codes = %{
 }
 
 code_abbrevs = %{
-  "Education Code" => "Educ Code"
+  "Business and Professions Code" => "Bus & Prof Code",
+  "Civil Code"                    => "Civ Code",
+  "Code of Civil Procedure"       => "Civ Proc Code",
+  "Commercial Code"               => "Com Code",
+  "Corporations Code"             => "Corp Code",
+  "Education Code"                => "Educ Code",
+  "Elections Code"                => "Elec Code",
+  "Evidence Code"                 => "Evid Code",
+  "Family Code"                   => "Fam Code",
+  "Financial Code"                => "Fin Code",
+  "Fish and Game Code"            => "Fish & Game Code",
+  "Government Code"               => "Govt Code",
+  "Harbors and Navigation Code"   => "Harb & Nav Code",
+  "Health and Safety Code"        => "Health & Safety Code",
+  "Insurance Code"                => "Ins Code",
+  "Labor Code"                    => "Lab Code",
+  "Military and Veterans Code"    => "Mil & Vet Code",
+  "Penal Code"                    => "Penal Code",
+  "Probate Code"                  => "Prob Code",
+  "Public Contract Code"          => "Pub Cont Code",
+  "Public Utilities Code"         => "Pub Util Code",
+  "Revenue and Taxation Code"     => "Rev & Tax Code",
+  "Streets and Highways Code"     => "Sts & High Code",
+  "Unemployment Insurance Code"   => "Unemp Ins Code",
+  "Vehicle Code"                  => "Veh Code",
+  "Water Code"                    => "Water Code",
+  "Welfare and Institutions Code" => "Welf & Inst Code",
 }
 
 
