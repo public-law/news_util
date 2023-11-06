30f7de2df1c56804b61f725cbad7c5504cb626ca
Robb Shecter <robb@public.law>
2023-11-06 00:05:27 -0700

    better error

--- lib/news_util.ex
+++ lib/news_util.ex
@@ -64,7 +64,7 @@ defmodule NewsUtil do
     "Welfare and Institutions Code" => "Welf & Inst Code",
   }
 
-  @pattern  ~r/sectionNum=[^&]+&lawCode=\w\w\w/
+  @pattern  ~r/sectionNum=[^&]+&lawCode=\w+|lawCode=[^&]+&sectionNum=[\d\.]+/
 
   @doc """
   Hello world.
