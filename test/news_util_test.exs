f35507bbf6855b169d113319439a27d19175c2ba
Robb Shecter <robb@public.law>
2023-11-06 00:19:54 -0700

    get another form (#1)
    * failing test
    
    * fixture func
    
    * test passes
    
    * update the script
    
    * failing test for new form
    
    * refactor
    
    * better error
    
    * test passes
    
    * refactor
    
    * refactor
    
    * refactor

--- test/news_util_test.exs
+++ test/news_util_test.exs
@@ -2,7 +2,18 @@ defmodule NewsUtilTest do
   use ExUnit.Case
   doctest NewsUtil
 
-  test "greets the world" do
-    assert NewsUtil.hello() == :world
+  def fixture(name) do
+    File.read!("test/fixtures/#{name}")
+  end
+
+
+  test "finds California citations when the leginfo links are standard" do
+    html = fixture("qandasec5.asp")
+    assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
+  end
+
+  test "finds California citations when the leginfo links have reversed params" do
+    html = fixture("qandasec6.asp")
+    assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605"]
   end
 end
