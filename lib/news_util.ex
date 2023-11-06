db7ca100679809ed368169ce6315e28193b03adc
Robb Shecter <robb@public.law>
2023-11-06 02:18:16 -0700

    refactor (#2)
    * add floki
    
    * code to make list of maps
    
    * finish
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor

--- lib/news_util.ex
+++ lib/news_util.ex
@@ -5,91 +5,97 @@ defmodule NewsUtil do
 
   @cal_codes %{
     "CONS" => "Constitution",
-    "BPC"  => "Business and Professions Code",
-    "CIV"  => "Civil Code",
-    "CCP"  => "Code of Civil Procedure",
-    "COM"  => "Commercial Code",
+    "BPC" => "Business and Professions Code",
+    "CIV" => "Civil Code",
+    "CCP" => "Code of Civil Procedure",
+    "COM" => "Commercial Code",
     "CORP" => "Corporations Code",
-    "EDC"  => "Education Code",
+    "EDC" => "Education Code",
     "ELEC" => "Elections Code",
     "EVID" => "Evidence Code",
-    "FAM"  => "Family Code",
-    "FIN"  => "Financial Code",
-    "FGC"  => "Fish and Game Code",
-    "GOV"  => "Government Code",
-    "HNC"  => "Harbors and Navigation Code",
-    "HSC"  => "Health and Safety Code",
-    "INS"  => "Insurance Code",
-    "LAB"  => "Labor Code",
-    "MVC"  => "Military and Veterans Code",
-    "PEN"  => "Penal Code",
+    "FAM" => "Family Code",
+    "FIN" => "Financial Code",
+    "FGC" => "Fish and Game Code",
+    "GOV" => "Government Code",
+    "HNC" => "Harbors and Navigation Code",
+    "HSC" => "Health and Safety Code",
+    "INS" => "Insurance Code",
+    "LAB" => "Labor Code",
+    "MVC" => "Military and Veterans Code",
+    "PEN" => "Penal Code",
     "PROB" => "Probate Code",
-    "PCC"  => "Public Contract Code",
-    "PUC"  => "Public Utilities Code",
-    "RTC"  => "Revenue and Taxation Code",
-    "SHC"  => "Streets and Highways Code",
-    "UIC"  => "Unemployment Insurance Code",
-    "VEH"  => "Vehicle Code",
-    "WAT"  => "Water Code",
-    "WIC"  => "Welfare and Institutions Code",
+    "PCC" => "Public Contract Code",
+    "PUC" => "Public Utilities Code",
+    "RTC" => "Revenue and Taxation Code",
+    "SHC" => "Streets and Highways Code",
+    "UIC" => "Unemployment Insurance Code",
+    "VEH" => "Vehicle Code",
+    "WAT" => "Water Code",
+    "WIC" => "Welfare and Institutions Code"
   }
 
   @code_abbrevs %{
     "Business and Professions Code" => "Bus & Prof Code",
-    "Civil Code"                    => "Civ Code",
-    "Code of Civil Procedure"       => "Civ Proc Code",
-    "Commercial Code"               => "Com Code",
-    "Corporations Code"             => "Corp Code",
-    "Education Code"                => "Educ Code",
-    "Elections Code"                => "Elec Code",
-    "Evidence Code"                 => "Evid Code",
-    "Family Code"                   => "Fam Code",
-    "Financial Code"                => "Fin Code",
-    "Fish and Game Code"            => "Fish & Game Code",
-    "Government Code"               => "Govt Code",
-    "Harbors and Navigation Code"   => "Harb & Nav Code",
-    "Health and Safety Code"        => "Health & Safety Code",
-    "Insurance Code"                => "Ins Code",
-    "Labor Code"                    => "Lab Code",
-    "Military and Veterans Code"    => "Mil & Vet Code",
-    "Penal Code"                    => "Penal Code",
-    "Probate Code"                  => "Prob Code",
-    "Public Contract Code"          => "Pub Cont Code",
-    "Public Utilities Code"         => "Pub Util Code",
-    "Revenue and Taxation Code"     => "Rev & Tax Code",
-    "Streets and Highways Code"     => "Sts & High Code",
-    "Unemployment Insurance Code"   => "Unemp Ins Code",
-    "Vehicle Code"                  => "Veh Code",
-    "Water Code"                    => "Water Code",
-    "Welfare and Institutions Code" => "Welf & Inst Code",
+    "Civil Code" => "Civ Code",
+    "Code of Civil Procedure" => "Civ Proc Code",
+    "Commercial Code" => "Com Code",
+    "Corporations Code" => "Corp Code",
+    "Education Code" => "Educ Code",
+    "Elections Code" => "Elec Code",
+    "Evidence Code" => "Evid Code",
+    "Family Code" => "Fam Code",
+    "Financial Code" => "Fin Code",
+    "Fish and Game Code" => "Fish & Game Code",
+    "Government Code" => "Govt Code",
+    "Harbors and Navigation Code" => "Harb & Nav Code",
+    "Health and Safety Code" => "Health & Safety Code",
+    "Insurance Code" => "Ins Code",
+    "Labor Code" => "Lab Code",
+    "Military and Veterans Code" => "Mil & Vet Code",
+    "Penal Code" => "Penal Code",
+    "Probate Code" => "Prob Code",
+    "Public Contract Code" => "Pub Cont Code",
+    "Public Utilities Code" => "Pub Util Code",
+    "Revenue and Taxation Code" => "Rev & Tax Code",
+    "Streets and Highways Code" => "Sts & High Code",
+    "Unemployment Insurance Code" => "Unemp Ins Code",
+    "Vehicle Code" => "Veh Code",
+    "Water Code" => "Water Code",
+    "Welfare and Institutions Code" => "Welf & Inst Code"
   }
-
-  @pattern  ~r/sectionNum=[^&]+&lawCode=[A-Z]+|lawCode=[^&]+&sectionNum=[\d\.]+/
-
 
   @spec find_citations(binary()) :: list()
   @doc """
   Find citations in a string of HTML.
   """
   def find_citations(html) do
-    Regex.scan(@pattern, html)
-    |> Enum.map(fn [match] ->
-        String.replace(match, "sectionNum=", "")
-        |> String.replace("lawCode=", "")
-        |> String.replace("&", " ")
-        |> String.split(" ")
-      end)
-    # Reverse the list if the lawCode is first
-    |> Enum.map(fn [law_code, section] ->
-        if String.match?(law_code, ~r/^[A-Z]/) do
-          [section, law_code]
-        else
-          [law_code, section]
-        end
-      end)
-    |> Enum.map(fn [section, code] -> {@code_abbrevs[@cal_codes[code]], String.replace_suffix(section, ".", "")} end)
-    |> Enum.map(fn {code, section} -> "CA #{code} Section #{section}" end)
+    {:ok, document} = Floki.parse_document(html)
+
+    leginfo_urls =
+      document
+      |> Floki.attribute("a", "href")
+      |> List.flatten()
+      |> Enum.map(&URI.parse/1)
+      |> Enum.filter(&leginfo_url?/1)
+
+    leginfo_urls
+    |> Enum.map(&leginfo_url_to_cite/1)
     |> Enum.sort()
     |> Enum.uniq()
+  end
+
+
+  defp leginfo_url?(%{host: "leginfo.legislature.ca.gov"}), do: true
+  defp leginfo_url?(_),  do: false
+
+  defp leginfo_url_to_cite(%{query: query}) do
+    query
+    |> URI.decode_query()
+    |> make_cite()
+  end
+
+  defp make_cite(%{"lawCode" => code, "sectionNum" => section}) do
+    "CA #{@code_abbrevs[@cal_codes[code]]} Section #{section}"
+    |> String.replace_suffix(".", "")
   end
 end
