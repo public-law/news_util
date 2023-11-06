856404715de91a49d9feccc2d97577e77fa09166
Robb Shecter <robb@public.law>
2023-11-05 23:44:25 -0700

    failing test

--- test/news_util_test.exs
+++ test/news_util_test.exs
@@ -5,4 +5,9 @@ defmodule NewsUtilTest do
   test "greets the world" do
     assert NewsUtil.hello() == :world
   end
+
+  test "finds California citations when the leginfo links are standard" do
+    html = fixture("qandasec5.asp")
+    assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
+  end
 end
