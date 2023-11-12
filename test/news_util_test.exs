1f6298d10c3415a97fc8069088dcdaba3ac18ab0
Robb Shecter <robb@public.law>
2023-11-12 16:44:34 -0700

    passing test for file retrieval

--- test/news_util_test.exs
+++ test/news_util_test.exs
@@ -2,14 +2,22 @@ defmodule NewsUtilTest do
   use ExUnit.Case
   doctest NewsUtil
 
+  def fixture_path(name) do
+    Path.join("test/fixtures", name)
+  end
+
   def fixture(name) do
-    File.read!("test/fixtures/#{name}")
+    File.read!(fixture_path(name))
   end
 
 
   test "finds California citations when the leginfo links are standard" do
     html = fixture("qandasec5.asp")
     assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
+  end
+
+  test "gets cites from a file directly" do
+    assert NewsUtil.find_citations(file_path: fixture_path("qandasec5.asp")) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
   end
 
   test "finds California citations when the leginfo links have reversed params" do
