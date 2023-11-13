4fafeef513ef65b2f10c7ffcd0d801f67af2941d
Robb Shecter <robb@public.law>
2023-11-12 17:42:19 -0700

    PDF  (#5)
    * refactor
    
    * add dialyxir
    
    * fix: dialyxir dep
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * feature: tmp_file function
    
    * more work on tmp_file
    
    * improvements
    
    * refactor: extract function
    
    * refactor: extract function
    
    * passing test for file retrieval
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * failing test for CO PDF
    
    * refactor
    
    * more code
    
    * refactor
    
    * read pdfs
    
    * catching one CRS cite
    
    * all tests pass

--- lib/news_util.ex
+++ lib/news_util.ex
@@ -1,62 +1,108 @@
+import Enum
+import List
 import CalCodes
 
+
 defmodule NewsUtil do
-  @spec find_citations(binary()) :: list()
+
   @doc """
-  Find citations in a string of HTML.
+  Find citations in a string of HTML or from a URL.
   """
-  def find_citations(html) do
-    html
-    |> uri_list()
-    |> Enum.map(&transform/1)
-    |> Enum.filter(&is_binary/1)
-    |> cleanup_list()
+  def find_citations(%URI{} = uri) do
+    url       = URI.to_string(uri)
+    temp_file = tmp_file!(url)
+    response  = HTTPoison.get!(url)
+    File.write!(temp_file, response.body)
+
+    find_citations(file: temp_file)
   end
 
-
-  defp transform(url) do
-    case url do
-      %{host: "leginfo.legislature.ca.gov"} ->
-        leginfo_url_to_cite(url)
-
-      %{host: "texas.public.law"} ->
-        texas_public_law_url_to_cite(url)
-
-      _ ->
-        nil
+  def find_citations(file: path) do
+    case Path.extname(path) do
+      ".pdf" -> find_citations(read_pdf_as_html!(path))
+      _      -> find_citations(File.read!(path))
     end
   end
 
+  def find_citations(html) when is_binary(html) do
+    cites_from_hrefs =
+      html
+      |> uri_list()
+      |> map(&transform/1)
+      |> filter(&is_binary/1)
+      |> cleanup_list()
 
-  defp uri_list(html) do
+    cites_from_text =
+      case Regex.scan(~r/(C.R.S. &#xa7;(&#xa7;)? \d+-\d+-\d+)/, html) do
+        list -> list |> flatten() |> uniq() |> map(fn m -> String.replace(m, ~r/&#xa7; ?/, "", global: true) end) |> reject(&(String.length(&1) == 0))
+      end
+
+     cites_from_hrefs  ++ cites_from_text
+  end
+
+
+  def read_pdf_as_html!(input_file) do
+    html_temp_file = tmp_file!("tempfile.html")
+    :os.cmd(String.to_charlist("mutool convert -o #{html_temp_file} #{input_file}"))
+    File.read!(html_temp_file)
+  end
+
+
+
+  @spec tmp_file!(binary()) :: binary()
+  def tmp_file!(ext_to_match \\ "tempfile.tmp") do
+    ext  = Path.extname(ext_to_match)
+    dir  = System.tmp_dir!()
+    file = "#{System.system_time()}-#{rand()}#{ext}"
+
+    Path.join(dir, file)
+  end
+
+
+  defp rand() do
+    :rand.uniform(10000000000000)
+  end
+
+
+  defp uri_list(html) when is_binary(html) do
     {:ok, document} = Floki.parse_document(html)
 
     document
     |> Floki.attribute("a", "href")
-    |> List.flatten()
-    |> Enum.map(&URI.parse/1)
+    |> flatten()
+    |> map(&URI.parse/1)
+  end
+
+
+  defp transform(%URI{} = url) do
+    case url do
+      %{host: "leginfo.legislature.ca.gov"} -> leginfo_url_to_cite(url)
+      %{host: "texas.public.law"}           -> texas_public_law_url_to_cite(url)
+
+      _ -> nil
+    end
   end
 
 
   defp cleanup_list(list) do
     list
-    |> Enum.sort()
-    |> Enum.uniq()
+    |> sort()
+    |> uniq()
   end
 
 
-  defp texas_public_law_url_to_cite(%{path: path}) do
+  defp texas_public_law_url_to_cite(%URI{path: path}) do
     path
     |> String.split("/")
-    |> List.last()
+    |> last()
     |> String.replace("_", " ")
     |> String.split(" ")
-    |> Enum.map(&String.capitalize/1)
-    |> Enum.join(" ")
+    |> map(&String.capitalize/1)
+    |> join(" ")
   end
 
 
-  defp leginfo_url_to_cite(%{query: query}) do
+  defp leginfo_url_to_cite(%URI{query: query}) do
     query
     |> URI.decode_query()
     |> make_cite_to_cal_codes()
--- test/news_util_test.exs
+++ test/news_util_test.exs
@@ -1,28 +1,40 @@
+import NewsUtil
+
 defmodule NewsUtilTest do
   use ExUnit.Case
   doctest NewsUtil
 
   def fixture(name) do
-    File.read!("test/fixtures/#{name}")
+    Path.join("test/fixtures", name)
   end
 
 
-  test "finds California citations when the leginfo links are standard" do
-    html = fixture("qandasec5.asp")
-    assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
+  test "California citations when the leginfo links are standard" do
+    filename = fixture("qandasec5.asp")
+    assert find_citations(file: filename) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
   end
 
-  test "finds California citations when the leginfo links have reversed params" do
-    html = fixture("qandasec6.asp")
-    assert NewsUtil.find_citations(html) == ["CA Educ Code Section 47605"]
+  test "California citations when the leginfo links have reversed params" do
+    filename = fixture("qandasec6.asp")
+    assert find_citations(file: filename) == ["CA Educ Code Section 47605"]
   end
 
-  test "finds Texas citations when they're in public.law links" do
-    html = fixture("Formal Marriage License | Fort Bend County.html")
-    assert NewsUtil.find_citations(html) == [
+  test "Texas citations when they're in public.law links" do
+    filename = fixture("Formal Marriage License | Fort Bend County.html")
+
+    assert find_citations(file: filename) == [
       "Tex. Fam. Code Section 2.003",
       "Tex. Fam. Code Section 2.013",
       "Tex. Fam. Code Section 2.203",
+    ]
+  end
+
+  test "Colorado CRS citations in a PDF" do
+    filename = fixture("JDF432.pdf")
+
+    assert find_citations(file: filename) == [
+      "C.R.S. 13-15-101",
+      "C.R.S. 13-15-102",
     ]
   end
 end
