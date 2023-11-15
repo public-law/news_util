eb22423d1cc7db682984334fb2dff2070c7bbef4
Robb Shecter <robb@public.law>
2023-11-14 21:57:13 -0700

    pin httpoison

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule NewsUtil.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:httpoison, "> 0.0.0"},
+      {:httpoison, "~> 2.2.0"},
       {:floki, "> 0.0.0"},
       {:dialyxir, "> 0.0.0", only: [:dev], runtime: false},
       {:credo, "> 0.0.0", only: [:dev], runtime: false},
