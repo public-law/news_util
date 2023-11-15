9e7cc1ae292018215e3d44b99493beb40d5cd007
Robb Shecter <robb@public.law>
2023-11-14 23:44:11 -0700

    refactor: extract Hex Package

--- lib/news_util.ex
+++ lib/news_util.ex
@@ -14,7 +14,7 @@ defmodule NewsUtil do
   def find_citations(%URI{} = uri) do
     url       = URI.to_string(uri)
     temp_file = FileUtil.tmp_file!(url)
-    File.write!(temp_file, Http.get!(url))
+    File.write!(temp_file, CurlEx.get!(url))
 
     find_citations_in_file(temp_file)
   end
