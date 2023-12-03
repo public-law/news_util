6cc61023d7bd5d31e3c8202fd541a439fafcbec4
Robb Shecter <robb@public.law>
2023-12-02 21:43:54 -0700

    extract function

--- test/news_util_test.exs
+++ test/news_util_test.exs
@@ -9,23 +9,23 @@ defmodule NewsUtilTest do
     Path.join("test/fixtures", name)
   end
 
+  def find_citations_in_fixture(name) do
+    find_citations_in_file(fixture(name))
+  end
+
 
   test "California citations when the leginfo links are standard" do
-    filename = fixture("qandasec5.asp")
-    assert find_citations_in_file(filename) == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
+    assert find_citations_in_fixture("qandasec5.asp") == ["CA Educ Code Section 47605", "CA Educ Code Section 47605.6"]
   end
 
 
   test "California citations when the leginfo links have reversed params" do
-    filename = fixture("qandasec6.asp")
-    assert find_citations_in_file(filename) == ["CA Educ Code Section 47605"]
+    assert find_citations_in_fixture("qandasec6.asp") == ["CA Educ Code Section 47605"]
   end
 
 
   test "Texas links to public.law" do
-    filename = fixture("Formal Marriage License | Fort Bend County.html")
-
-    assert find_citations_in_file(filename) == [
+    assert find_citations_in_fixture("Formal Marriage License | Fort Bend County.html") == [
       "Tex. Fam. Code Section 2.003",
       "Tex. Fam. Code Section 2.013",
       "Tex. Fam. Code Section 2.203",
