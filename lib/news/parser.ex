1a9b0eb7e4f25546b688db83333307ad859ee365
Robb Shecter <robb@public.law>
2023-12-06 20:18:31 -0700

    checkpoint

--- lib/news/parser.ex
+++ lib/news/parser.ex
@@ -8,6 +8,8 @@ defmodule News.Parser do
   """
   @spec find_title(binary) :: binary
   def find_title(html) do
+    {:ok, document} = Floki.parse_document(html)
+
     # title_from_meta_tags = find_title_from_meta_tags(html)
     # title_from_html_tag  = find_title_from_html_tag(html)
 
@@ -17,26 +19,31 @@ defmodule News.Parser do
     #   title_from_html_tag
     # end
 
-    find_title_from_html_tag(html)
+    find_title_from_html_tag(document)
   end
+
 
   # Create initial simple implementations of the missing functions.
   def find_title_from_meta_tags(_html) do
     "Charter School FAQ Section 99"
   end
 
-  def find_title_from_html_tag(html) do
-    {:ok, document} = Floki.parse_document(html)
 
-    title = raw_title(document)
+  def find_title_from_html_tag(document) do
+    orig_title  = raw_title(document)
+    clean_title = title_without_hyphenation(orig_title)
 
-    title_without_hyphenation =
-      title
+    # TODO: Decide intelligently which title to return.
+
+    clean_title
+  end
+
+
+  defp title_without_hyphenation(title) do
+    title
       |> String.split(~r/[-–—]/)
       |> List.first
       |> String.trim
-
-    title_without_hyphenation
   end
 
 
