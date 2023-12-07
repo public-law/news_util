ead61c7f0dc23591bed75a72d02e99e63e6a9a23
Robb Shecter <robb@public.law>
2023-12-06 22:40:58 -0700

    checkpoint

--- test/news/parser_test.exs
+++ test/news/parser_test.exs
@@ -8,13 +8,17 @@ defmodule News.ParserTest do
 
   @test_cases_for_title [
     %{
-      file: "qandasec5.asp",
+      file:  "qandasec5.asp",
+      url:   "https://www.cde.ca.gov/sp/ch/qandasec5.asp",
+
       title: "Charter School FAQ Section 5",
       source_name: "California Department of Education",
       source_url: "https://www.cde.ca.gov",
     },
     %{
-      file: "qandasec6.asp",
+      file:  "qandasec6.asp",
+      url:   "https://www.cde.ca.gov/sp/ch/qandasec6.asp",
+
       title: "Charter School FAQ Section 6",
       source_name: "California Department of Education",
       source_url: "https://www.cde.ca.gov",
@@ -22,13 +26,13 @@ defmodule News.ParserTest do
   ]
 
 
-  Enum.each(@test_cases_for_title, fn %{file: f, title: t, source_name: source_name, source_url: source_url} ->
+  Enum.each(@test_cases_for_title, fn %{file: f, url: url, title: title, source_name: source_name, source_url: source_url} ->
     test "finds the title in #{f}" do
       {:ok, document} = Floki.parse_document(File.read!(Test.fixture(unquote f)))
 
-      assert Parser.find_title(document)       == unquote(t)
-      assert Parser.find_source_name(document) == unquote(source_name)
-      assert Parser.find_source_url(document)  == unquote(source_url)
+      assert Parser.find_title(document)          == unquote(title)
+      assert Parser.find_source_url(unquote url)  == unquote(source_url)
+      assert Parser.find_source_name(unquote url) == unquote(source_name)
     end
   end)
 end
