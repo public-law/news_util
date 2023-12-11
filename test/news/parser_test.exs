c3c4b12691de6cf50872c39762fe26b0e39d01f5
Robb Shecter <robb@public.law>
2023-12-11 14:25:54 -0700

    refactor: formatting

--- test/news/parser_test.exs
+++ test/news/parser_test.exs
@@ -6,24 +6,13 @@ defmodule News.ParserTest do
   use ExUnit.Case
   doctest News.Parser
 
-  @test_cases [
-    %{
-      file:  "qandasec5.asp",
-      url:   "https://www.cde.ca.gov/sp/ch/qandasec5.asp",
 
-      title:      "Charter School FAQ Section 5",
-      source_url: "https://www.cde.ca.gov",
-    },
-    %{
-      file:  "qandasec6.asp",
-      url:   "https://www.cde.ca.gov/sp/ch/qandasec6.asp",
-
-      title:      "Charter School FAQ Section 6",
-      source_url: "https://www.cde.ca.gov",
-    },
+  @title_and_source_url_test_cases [
+    %{file: "qandasec5.asp", url: "https://www.cde.ca.gov/sp/ch/qandasec5.asp", title: "Charter School FAQ Section 5", source_url: "https://www.cde.ca.gov"},
+    %{file: "qandasec6.asp", url: "https://www.cde.ca.gov/sp/ch/qandasec6.asp", title: "Charter School FAQ Section 6", source_url: "https://www.cde.ca.gov"}
   ]
 
-  Enum.each(@test_cases, fn %{file: f, url: url, title: title, source_url: source_url} ->
+  Enum.each(@title_and_source_url_test_cases, fn %{file: f, url: url, title: title, source_url: source_url} ->
     test "finds the title in #{f}" do
       file = unquote(f)
       {:ok, document} = file |> Test.fixture |> File.read! |> Floki.parse_document
@@ -35,24 +24,9 @@ defmodule News.ParserTest do
 
 
   @source_name_test_cases [
-    %{
-      file: "article279569109.html",
-      url:  "https://www.star-telegram.com/news/state/texas/article279569109.html",
-
-      source_name: "Fort Worth Star-Telegram",
-    },
-    %{
-      file: "legal-recourse-for-victims-of-san-antonio-street-racing-accident.html",
-      url:  "https://www.carabinshaw.com/legal-recourse-for-victims-of-san-antonio-street-racing-accident.html",
-
-      source_name: "Carabin Shaw",
-    }
-    # %{
-    #   file:  "qandasec6.asp",
-    #   url:   "https://www.cde.ca.gov/sp/ch/qandasec6.asp",
-
-    #   source_name: "California Department of Education",
-    # },
+    %{file: "article279569109.html", url: "https://www.star-telegram.com/news/state/texas/article279569109.html",                              source_name: "Fort Worth Star-Telegram"},
+    %{file: "street-racing.html",    url: "https://www.carabinshaw.com/legal-recourse-for-victims-of-san-antonio-street-racing-accident.html", source_name: "Carabin Shaw"},
+    %{file: "qandasec6.asp",         url: "https://www.cde.ca.gov/sp/ch/qandasec6.asp",                                                        source_name: "California Department of Education"}
   ]
 
   Enum.each(@source_name_test_cases, fn %{file: f, url: u, source_name: s} ->
